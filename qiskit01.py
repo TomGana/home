7de60d67426d3b2fa9d1fe0890c512b92b7b0b31
TomGana <tgana@stevens.edu>
2020-11-02 19:21:27 -0500

    init

--- qiskit01.py
+++ qiskit01.py
@@ -4,7 +4,7 @@ import matplotlib
 circuit = q.QuantumCircuit(2,2) #(qbits, bits)
 #(0,0)
 circuit.x(0) #(1,0)
-circuit.cx() #cnot, flips 2nd qbit if first qbit is a 1
+circuit.cx(0,1) #cnot, flips 2nd qbit if first qbit is a 1
 #(1,1)
 circuit.measure([0,1], [0,1]) #map to bit
 
