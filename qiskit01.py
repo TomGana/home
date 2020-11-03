8a0dc095380b4fdd7bbdc3a20cdfd2c6ea2b6043
TomGana <tgana@stevens.edu>
2020-11-02 19:30:48 -0500

    init

--- qiskit01.py
+++ qiskit01.py
@@ -10,13 +10,13 @@ circuit.measure([0,1], [0,1]) #map to bit
 
 circuit.draw()
 
-
+#circuit.draw(output='mpl')
 
 from qiskit import IBMQ
 
 IBMQ.save_account("b3bb883ac33e8faef5af38947e19a659b7ec8a265cba863a3c1a667c72d8826b34d19b3ec3d312109b0ed7edbe416bb5fb069b979db02b45e7d04fc0fb919941")
 #IBMQ.load_account()
-provider = IBMQ.get_provider("ibm-q")
+provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
 for backend in provider.backends():
     try:
         qubit_count = len(backend.properties().qubits)
